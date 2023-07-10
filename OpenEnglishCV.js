f0ea6d0f4d0a6010b094caaa39a2e283806787cf
Rubén <98332317+marsi107@users.noreply.github.com>
2023-07-11 00:01:43 +0200

    upload js to open CVs

--- OpenEnglishCV.js
+++ OpenEnglishCV.js
@@ -1,16 +1,11 @@
-// Get the buttons by their class name
-const resumeButtonEn = document.getElementsByClassName("resume-button-en");
+// JavaScript code
+document.addEventListener('DOMContentLoaded', function() {
+  // Get the English CV button element
+  var englishCVButton = document.querySelector('.resume-button-en');
 
-// Function to handle the button click event
-function handleEnButtonClick(event) {
-  // Prevent the default button behavior
-  event.preventDefault();
-  console.log("Funct entered-------");
-  // Open the CV document in a new tab
-  window.open("/resources/cv.html"); // Replace with the actual path to your CV document
-}
-
-// Attach the event handler to each button
-Array.from(resumeButtonEn).forEach((button) => {
-  button.addEventListener("click", handleEnButtonClick);
+  // Add click event listener to the button
+  englishCVButton.addEventListener('click', function() {
+    // Open the English CV document in a new tab
+    window.open('CV-Ruben-EN.pdf', '_blank');
+  });
 });
--- OpenSpanishCV.js
+++ OpenSpanishCV.js
@@ -1,16 +1,11 @@
-// Get the buttons by their class name
-const resumeButtonEs = document.getElementsByClassName("resume-button-en");
+// JavaScript code
+document.addEventListener('DOMContentLoaded', function() {
+  // Get the Spanish CV button element
+  var spanishCVButton = document.querySelector('.resume-button-es');
 
-// Function to handle the button click event
-function handleEsButtonClick(event) {
-  // Prevent the default button behavior
-  event.preventDefault();
-  
-  // Open the CV document in a new tab
-  window.open("/resources/cv.html"); // Replace with the actual path to your CV document
-}
-
-// Attach the event handler to each button
-Array.from(resumeButtonEs).forEach((button) => {
-  button.addEventListener("click", handleEsButtonClick);
+  // Add click event listener to the button
+  spanishCVButton.addEventListener('click', function() {
+    // Open the Spanish CV document in a new tab
+    window.open('CV-Ruben-ES.pdf', '_blank');
+  });
 });
