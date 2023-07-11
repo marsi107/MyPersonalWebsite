ff0c8446d416bad74b3f6476a349287249bc6c1e
Rubén <98332317+marsi107@users.noreply.github.com>
2023-07-11 19:40:56 +0200

    Unify OpenCV JS

--- OpenCVs.js
+++ OpenCVs.js
@@ -1,7 +1,15 @@
 // JavaScript code
 document.addEventListener('DOMContentLoaded', function() {
+  // Get the English CV button element
+  var englishCVButton = document.querySelector('.resume-button-en');
   // Get the Spanish CV button element
   var spanishCVButton = document.querySelector('.resume-button-es');
+
+  // Add click event listener to the button
+  englishCVButton.addEventListener('click', function() {
+    // Open the English CV document in a new tab
+    window.open('resources/CV-Ruben-EN.pdf', '_blank');
+  });
 
   // Add click event listener to the button
   spanishCVButton.addEventListener('click', function() {
