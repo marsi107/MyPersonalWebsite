b5dbb2d2e688fa79173ff523ad0436dc4f95f781
Rubén <98332317+marsi107@users.noreply.github.com>
2023-10-10 13:51:16 +0200

    Add icons to NavBar and adapt styling

--- portfolio/app/components/NavBar.tsx
+++ portfolio/app/components/NavBar.tsx
@@ -1,19 +1,18 @@
 import React from 'react'
+import { MoonIcon, Bars3Icon } from '@heroicons/react/24/outline'
 
 const NavBar = () => {
     return (
         <header className="absolute items-center h-[45px] w-screen flex bg-defColors-second dark:bg-defColors-second_dark font-rob">
             <div className="grow"></div>
-            <div>DD</div>
-            <div className="flex space-x-3 pl-6">
+            <Bars3Icon className="h-6" />
+            <div className="flex space-x-6 pl-6">
                 <div className="text-lg xl:text-xl">Home</div>
                 <div className="text-lg xl:text-xl">Portfolio</div>
                 <div className="text-lg xl:text-xl">Resume</div>
                 <div className="text-lg xl:text-xl">Contact</div>
             </div>
-            <div className="pl-6">
-                DM
-            </div>
+            <MoonIcon className="h-5 pl-6" />
             <div className="grow"></div>
         </header>
     )
