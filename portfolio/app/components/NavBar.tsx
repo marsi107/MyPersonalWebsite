ef3db7a1dc4d301826c7b06a327d3e2ec087c56a
Rubén <98332317+marsi107@users.noreply.github.com>
2023-10-10 13:36:53 +0200

    Add basic NavBar structure

--- portfolio/app/components/NavBar.tsx
+++ portfolio/app/components/NavBar.tsx
@@ -2,13 +2,19 @@ import React from 'react'
 
 const NavBar = () => {
     return (
-        <header className="absolute h-[45px] w-screen flex bg-defColors-second dark:bg-defColors-second_dark font-rob">
-            <div className="flex">
-                <div className="text-lg xl:text-xl pt-2 pl-2">Home</div>
-                <div className="text-lg xl:text-xl pt-2 pl-2">Portfolio</div>
-                <div className="text-lg xl:text-xl pt-2 pl-2">Resume</div>
-                <div className="text-lg xl:text-xl pt-2 pl-2">Contact</div>
+        <header className="absolute items-center h-[45px] w-screen flex bg-defColors-second dark:bg-defColors-second_dark font-rob">
+            <div className="grow"></div>
+            <div>DD</div>
+            <div className="flex space-x-3 pl-6">
+                <div className="text-lg xl:text-xl">Home</div>
+                <div className="text-lg xl:text-xl">Portfolio</div>
+                <div className="text-lg xl:text-xl">Resume</div>
+                <div className="text-lg xl:text-xl">Contact</div>
             </div>
+            <div className="pl-6">
+                DM
+            </div>
+            <div className="grow"></div>
         </header>
     )
 }
