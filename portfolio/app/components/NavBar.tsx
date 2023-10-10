acb362de47f6e49f4221e98a799ebe82ca5fa81a
Rubén <98332317+marsi107@users.noreply.github.com>
2023-10-10 21:07:55 +0200

    make navbar responsible

--- portfolio/app/components/NavBar.tsx
+++ portfolio/app/components/NavBar.tsx
@@ -5,14 +5,15 @@ const NavBar = () => {
     return (
         <header className="absolute items-center h-[45px] w-screen flex bg-defColors-second dark:bg-defColors-second_dark font-rob">
             <div className="grow"></div>
-            <Bars3Icon className="h-6" />
-            <div className="flex space-x-6 pl-6">
-                <div className="text-lg xl:text-xl">Home</div>
-                <div className="text-lg xl:text-xl">Portfolio</div>
-                <div className="text-lg xl:text-xl">Resume</div>
-                <div className="text-lg xl:text-xl">Contact</div>
+            <Bars3Icon className="lg:hidden h-10 pl-6" />
+            <div className="flex pl-6 space-x-6 text-lg lg:space-x-12 lg:text-2xl">
+                <div>Home</div>
+                <div>Portfolio</div>
+                <div className="max-lg:hidden">Tech Stack</div>
+                <div>Resume</div>
+                <div>Contact</div>
             </div>
-            <MoonIcon className="h-5 pl-6" />
+            <MoonIcon className="h-10 pl-6 pr-6" />
             <div className="grow"></div>
         </header>
     )
--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -4,7 +4,7 @@ import { NavBar, Portfolio, TechStack, Resume, Contact } from "./components"
 
 const HomePage = () => {
   return (
-    <div className="h-screen bg-defColors-first text-defColors-text dark:bg-defColors-first_dark dark:text-defColors-text_dark font-noto">
+    <div className="h-full bg-defColors-first text-defColors-text dark:bg-defColors-first_dark dark:text-defColors-text_dark font-noto">
       <NavBar />
       <div className="pt-20 pl-5">
         <div>
