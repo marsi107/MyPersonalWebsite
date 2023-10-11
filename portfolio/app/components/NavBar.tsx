70206c786220c0e5b77f14238d44f3294d4c697c
Rubén <98332317+marsi107@users.noreply.github.com>
2023-10-11 22:36:49 +0200

    Fix responsive design for phones

--- portfolio/app/components/NavBar.tsx
+++ portfolio/app/components/NavBar.tsx
@@ -5,15 +5,15 @@ const NavBar = () => {
     return (
         <header className="fixed items-center h-[50px] w-screen flex bg-defColors-second dark:bg-defColors-second_dark font-rob lg:h-[60px]">
             <div className="grow"></div>
-            <Bars3Icon className="h-[40px] pl-6 sm:pr-9 sm:h-9 lg:hidden" />
-            <div className="flex space-x-6 text-xl sm:text-2xl sm:space-x-9 lg:space-x-16">
+            <Bars3Icon className="h-16 pl-6 sm:pr-9 sm:h-9 lg:hidden" />
+            <div className="flex space-x-6 text-lg sm:text-2xl sm:space-x-9 lg:space-x-16">
                 <div className="max-sm:hidden">Home</div>
                 <div>Portfolio</div>
                 <div className="max-lg:hidden">Tech Stack</div>
                 <div>Resume</div>
                 <div>Contact</div>
             </div>
-            <MoonIcon className="h-[40px] pl-6 pr-6 sm:h-8 sm:pl-9 lg:h-10 lg:pl-16" />
+            <MoonIcon className="h-14 pl-6 pr-6 sm:h-8 sm:pl-9 lg:h-10 lg:pl-16" />
             <div className="grow"></div>
         </header>
     )
