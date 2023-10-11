72a753d6ba19ca6907f550a3dbdbb51ae9d73931
Rubén <98332317+marsi107@users.noreply.github.com>
2023-10-11 19:54:27 +0200

    Make navbar responsive for phones

--- portfolio/app/components/NavBar.tsx
+++ portfolio/app/components/NavBar.tsx
@@ -3,17 +3,17 @@ import { MoonIcon, Bars3Icon } from '@heroicons/react/24/outline'
 
 const NavBar = () => {
     return (
-        <header className="absolute items-center h-[45px] w-screen flex bg-defColors-second dark:bg-defColors-second_dark font-rob">
+        <header className="absolute items-center h-[50px] w-screen flex bg-defColors-second dark:bg-defColors-second_dark font-rob">
             <div className="grow"></div>
-            <Bars3Icon className="h-7 pl-3 lg:hidden" />
-            <div className="flex pl-3 space-x-3 text-lg lg:space-x-12 lg:text-2xl">
-                <div>Home</div>
+            <Bars3Icon className="h-8 pl-6 lg:hidden" />
+            <div className="flex space-x-6 text-xl lg:space-x-12 lg:text-2xl">
+                <div className="max-sm:hidden">Home</div>
                 <div>Portfolio</div>
                 <div className="max-lg:hidden">Tech Stack</div>
                 <div>Resume</div>
                 <div>Contact</div>
             </div>
-            <MoonIcon className="h-7 pl-3 pr-3 lg:h-10 lg:pl-6" />
+            <MoonIcon className="h-7 pl-6 pr-6 lg:h-10 lg:pl-6" />
             <div className="grow"></div>
         </header>
     )
