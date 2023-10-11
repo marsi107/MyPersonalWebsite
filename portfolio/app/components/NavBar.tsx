9c29bf04a524059b6308175bdb24d18a9c9d9e00
Rubén <98332317+marsi107@users.noreply.github.com>
2023-10-11 22:28:01 +0200

    test icons with size in pixels

--- portfolio/app/components/NavBar.tsx
+++ portfolio/app/components/NavBar.tsx
@@ -5,7 +5,7 @@ const NavBar = () => {
     return (
         <header className="fixed items-center h-[50px] w-screen flex bg-defColors-second dark:bg-defColors-second_dark font-rob lg:h-[60px]">
             <div className="grow"></div>
-            <Bars3Icon className="h-10 pl-6 sm:pr-9 sm:h-9 lg:hidden" />
+            <Bars3Icon className="h-[40px] pl-6 sm:pr-9 sm:h-9 lg:hidden" />
             <div className="flex space-x-6 text-xl sm:text-2xl sm:space-x-9 lg:space-x-16">
                 <div className="max-sm:hidden">Home</div>
                 <div>Portfolio</div>
@@ -13,7 +13,7 @@ const NavBar = () => {
                 <div>Resume</div>
                 <div>Contact</div>
             </div>
-            <MoonIcon className="h-10 pl-6 pr-6 sm:h-8 sm:pl-9 lg:h-10 lg:pl-16" />
+            <MoonIcon className="h-[40px] pl-6 pr-6 sm:h-8 sm:pl-9 lg:h-10 lg:pl-16" />
             <div className="grow"></div>
         </header>
     )
