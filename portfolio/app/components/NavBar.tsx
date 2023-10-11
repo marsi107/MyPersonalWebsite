0e4188e0bac1c39ef136e81d6ab73e281f5bde6e
Rubén <98332317+marsi107@users.noreply.github.com>
2023-10-11 20:20:26 +0200

    make the navbar follow the scroll

--- portfolio/app/components/NavBar.tsx
+++ portfolio/app/components/NavBar.tsx
@@ -3,7 +3,7 @@ import { MoonIcon, Bars3Icon } from '@heroicons/react/24/outline'
 
 const NavBar = () => {
     return (
-        <header className="absolute items-center h-[50px] w-screen flex bg-defColors-second dark:bg-defColors-second_dark font-rob lg:h-[60px]">
+        <header className="fixed items-center h-[50px] w-screen flex bg-defColors-second dark:bg-defColors-second_dark font-rob lg:h-[60px]">
             <div className="grow"></div>
             <Bars3Icon className="h-10 pl-6 sm:pr-9 sm:h-9 lg:hidden" />
             <div className="flex space-x-6 text-xl sm:text-2xl sm:space-x-9 lg:space-x-16">
