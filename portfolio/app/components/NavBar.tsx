ba46297320f0393e3c25a13d5d5448973fcb7a17
Rubén <98332317+marsi107@users.noreply.github.com>
2023-10-11 20:16:06 +0200

    Make NavBar completely responsive

--- portfolio/app/components/NavBar.tsx
+++ portfolio/app/components/NavBar.tsx
@@ -3,17 +3,17 @@ import { MoonIcon, Bars3Icon } from '@heroicons/react/24/outline'
 
 const NavBar = () => {
     return (
-        <header className="absolute items-center h-[50px] w-screen flex bg-defColors-second dark:bg-defColors-second_dark font-rob">
+        <header className="absolute items-center h-[50px] w-screen flex bg-defColors-second dark:bg-defColors-second_dark font-rob lg:h-[60px]">
             <div className="grow"></div>
-            <Bars3Icon className="h-8 pl-6 lg:hidden" />
-            <div className="flex space-x-6 text-xl lg:space-x-12 lg:text-2xl">
+            <Bars3Icon className="h-10 pl-6 sm:pr-9 sm:h-9 lg:hidden" />
+            <div className="flex space-x-6 text-xl sm:text-2xl sm:space-x-9 lg:space-x-16">
                 <div className="max-sm:hidden">Home</div>
                 <div>Portfolio</div>
                 <div className="max-lg:hidden">Tech Stack</div>
                 <div>Resume</div>
                 <div>Contact</div>
             </div>
-            <MoonIcon className="h-7 pl-6 pr-6 lg:h-10 lg:pl-6" />
+            <MoonIcon className="h-10 pl-6 pr-6 sm:h-8 sm:pl-9 lg:h-10 lg:pl-16" />
             <div className="grow"></div>
         </header>
     )
