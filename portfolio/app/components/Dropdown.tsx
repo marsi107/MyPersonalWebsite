ebce2071c155b735edabfc96d9a35eeeab2b8593
Rubén <98332317+marsi107@users.noreply.github.com>
2023-10-12 23:36:46 +0200

    add dynamic styling to dropdowns

--- portfolio/app/components/Dropdown.tsx
+++ portfolio/app/components/Dropdown.tsx
@@ -13,24 +13,28 @@ interface DropdownProps {
 
 export default function Dropdown({ sections, iconIndex }: DropdownProps) {
 
+    let menuItemClassName = "absolute z-10 mt-2 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+
     const checkDropdownIcon = (iconIndex: number) => {
         switch (iconIndex) {
             case 0:
+                menuItemClassName += " left-0 w-24"
                 return <Bars3Icon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
 
             case 1:
+                menuItemClassName += " right-0 w-20"
                 return <MoonIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
 
             default:
+                menuItemClassName += " left-0 w-24"
                 return <Bars3Icon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
         }
-
     }
 
     return (
         <Menu as="div" className="relative inline-block text-left">
             <div>
-                <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                <Menu.Button className="inline-flex w-full justify-center rounded-md px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
                     {checkDropdownIcon(iconIndex)}
                 </Menu.Button>
             </div>
@@ -44,7 +48,7 @@ export default function Dropdown({ sections, iconIndex }: DropdownProps) {
                 leaveFrom="transform opacity-100 scale-100"
                 leaveTo="transform opacity-0 scale-95"
             >
-                <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <Menu.Items className={menuItemClassName}>
                     <div className="py-1">
                         {
                             sections.map((section, index) => (
--- portfolio/app/components/NavBar.tsx
+++ portfolio/app/components/NavBar.tsx
@@ -19,19 +19,25 @@ const NavBar = () => {
     // <Bars3Icon className="cursor-pointer h-16 pl-6 sm:pr-9 sm:h-9 lg:hidden" />
     // <Bars3Icon className="cursor-pointer hover:bg-slate-500 focus:outline-none h-16 pl-6 sm:pr-9 sm:h-9 lg:hidden" onClick={toggleMenuDropdown} />
     // <MoonIcon className="cursor-pointer h-14 pl-6 pr-6 sm:h-8 sm:pl-9 lg:h-10 lg:pl-16" onClick={toggleDarkModeDropdown} />
+
+    // className="pl-6 sm:pr-9 lg:hidden"
     return (
         <header className="fixed items-center h-[50px] w-screen flex bg-defColors-second dark:bg-defColors-second_dark font-rob lg:h-[60px]">
             <div className="grow"></div>
-            <Dropdown sections={menuSections} iconIndex={0} />
+            <div className="pl-6 sm:pr-9 lg:hidden">
+                <Dropdown sections={menuSections} iconIndex={0} />
+            </div>
             <div className="flex space-x-6 text-lg sm:text-2xl sm:space-x-9 lg:space-x-16">
-                <a href="#Home" className="hover:bg-slate-500 max-sm:hidden" onClick={scrollToSection}>Home</a>
+                <a href="#Home" className="hover:bg-gray-500 max-sm:hidden" onClick={scrollToSection}>Home</a>
                 <a href="#AboutMe" className="max-lg:hidden" onClick={scrollToSection}>About Me</a>
                 <a href="#Portfolio" onClick={scrollToSection}>Portfolio</a>
                 <a href="#TechStack" className="max-lg:hidden" onClick={scrollToSection}>Tech Stack</a>
                 <a href="#Resume" onClick={scrollToSection}>Resume</a>
                 <a href="#Contact" onClick={scrollToSection}>Contact</a>
             </div>
-            <Dropdown sections={darkModeSections} iconIndex={1} />
+            <div className='pl-6 pr-6 sm:pl-9 lg:pl-16'>
+                <Dropdown sections={darkModeSections} iconIndex={1} />    
+            </div>
             <div className="grow"></div>
         </header>
     )
