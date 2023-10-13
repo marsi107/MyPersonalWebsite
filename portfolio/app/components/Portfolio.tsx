13f92f424f8999baead6508fbbb3e6588ba7991c
Rubén <98332317+marsi107@users.noreply.github.com>
2023-10-13 13:10:15 +0200

    Add swiper

--- portfolio/app/components/Portfolio.tsx
+++ portfolio/app/components/Portfolio.tsx
@@ -1,10 +1,35 @@
+"use client"
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Autoplay } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
 
 const Portfolio = () => {
     return (
         <div id="Portfolio">
             <h2 className="headers2">Portfolio</h2>
-            <p>some dummy text for portfolio ---------------------------------------- </p>
+            <div className="mr-5">
+                <Swiper
+                    loop={true}
+                    spaceBetween={1}
+                    navigation={true}
+                    modules={[Navigation, Autoplay]}
+                    autoplay={{
+                        delay: 5000,
+                        disableOnInteraction: false,
+                    }}
+                    className="h-[50%] text-defColors-main"
+                >
+                    <SwiperSlide>
+                        <img src={"../../images/amazon-clone.PNG"} />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={"../../images/Portfolio.jpg"} />
+                    </SwiperSlide>
+                </Swiper>
+            </div>
         </div>
     )
 }
