ae24c48c6d2e9f6e63d393cead33274a2d45d21a
Rubén <98332317+marsi107@users.noreply.github.com>
2023-10-13 12:46:19 +0200

    Add class for headers

--- portfolio/app/components/Portfolio.tsx
+++ portfolio/app/components/Portfolio.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Portfolio = () => {
     return (
         <div id="Portfolio">
+            <h2 className="headers2">Portfolio</h2>
             <p>some dummy text for portfolio ---------------------------------------- </p>
         </div>
     )
