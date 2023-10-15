0e46f40f2b9a42307e5823303eb1ada73f117286
Rubén <98332317+marsi107@users.noreply.github.com>
2023-10-15 15:32:46 +0200

    Update resume section

--- portfolio/app/components/Resume.tsx
+++ portfolio/app/components/Resume.tsx
@@ -1,9 +1,23 @@
+"use client"
 import React from 'react'
 
 const Resume = () => {
+
     return (
         <div id="Resume">
-            Resume
+            <h2 className="headers2">Resume</h2>
+            <div className="flex space-x-6">
+                <a href="../resources/CV-Ruben-EN.pdf" target="_blank" rel="noopener noreferrer"
+                    className="bg-defColors-lGray text-defColors-dBG dark:bg-defColors-dGray dark:text-defColors-lBG hover:text-defColors-main hover:font-semibold dark:hover:text-defColors-main customRing px-3 py-2 rounded-md shadow-lg"
+                >
+                    English CV
+                </a>
+                <a href="../resources/CV-Ruben-ES.pdf" target="_blank" rel="noopener noreferrer"
+                    className="bg-defColors-lGray text-defColors-dBG dark:bg-defColors-dGray dark:text-defColors-lBG hover:text-defColors-main hover:font-semibold dark:hover:text-defColors-main customRing px-3 py-2 rounded-md shadow-lg"
+                >
+                    Spanish CV
+                </a>
+            </div>
         </div>
     )
 }
