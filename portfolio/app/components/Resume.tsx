9c29c6001e2d434d47a0287fcbc0fafea727aee5
Rubén <98332317+marsi107@users.noreply.github.com>
2023-10-15 19:50:28 +0200

    remove font semibold from hover

--- portfolio/app/components/Resume.tsx
+++ portfolio/app/components/Resume.tsx
@@ -8,12 +8,12 @@ const Resume = () => {
             <h2 className="headers2">Resume</h2>
             <div className="flex space-x-6">
                 <a href="../resources/CV-Ruben-EN.pdf" target="_blank" rel="noopener noreferrer"
-                    className="bg-defColors-lGray text-defColors-dBG dark:bg-defColors-dGray dark:text-defColors-lBG hover:text-defColors-main hover:font-semibold dark:hover:text-defColors-main customRing px-3 py-2 rounded-md shadow-lg"
+                    className="bg-defColors-lGray text-defColors-dBG dark:bg-defColors-dGray dark:text-defColors-lBG hover:text-defColors-main dark:hover:text-defColors-main customRing px-3 py-2 rounded-md shadow-lg"
                 >
                     English CV
                 </a>
                 <a href="../resources/CV-Ruben-ES.pdf" target="_blank" rel="noopener noreferrer"
-                    className="bg-defColors-lGray text-defColors-dBG dark:bg-defColors-dGray dark:text-defColors-lBG hover:text-defColors-main hover:font-semibold dark:hover:text-defColors-main customRing px-3 py-2 rounded-md shadow-lg"
+                    className="bg-defColors-lGray text-defColors-dBG dark:bg-defColors-dGray dark:text-defColors-lBG hover:text-defColors-main dark:hover:text-defColors-main customRing px-3 py-2 rounded-md shadow-lg"
                 >
                     Spanish CV
                 </a>
