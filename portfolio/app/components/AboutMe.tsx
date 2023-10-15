1855efd792c607166e03d18732ac2a8a8262287d
Rubén <98332317+marsi107@users.noreply.github.com>
2023-10-15 18:04:44 +0200

    Update about me section

--- portfolio/app/components/AboutMe.tsx
+++ portfolio/app/components/AboutMe.tsx
@@ -4,19 +4,13 @@ const AboutMe = () => {
     return (
         <div id="AboutMe">
             <h2 className="headers2">About Me</h2>
-            <div className="bg-black bg-opacity-5 border-l-2 dark:bg-opacity-20 p-6 border-l-defColors-main dark:border-l-[1px]">
+            <div className="max-w-6xl bg-black bg-opacity-5 border-l-2 dark:bg-opacity-20 p-6 border-l-defColors-main dark:border-l-[1px]">
                 <img src="../images/profile.png" alt="" className="float-left mr-6 mb-1 w-32 rounded-full" />
-                <p className=" text-defColors-dBG dark:text-defColors-lBG">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Et asperiores hic corrupti debitis quae maiores doloremque dolore sequi iure rerum praesentium culpa, qui nobis ipsum, at facilis distinctio magni quod tempore impedit porro! Neque, nobis sunt?
-                    Accusamus commodi alias ea laudantium eaque eligendi earum hic, eos voluptates officiis cumque, debitis repellendus.
-                    Beatae, quas minus assumenda illo sapiente possimus voluptate?
-                    Expedita, saepe sit est rerum id nihil vel voluptate totam, corrupti harum unde ipsa tempore dolorem beatae adipisci quas dolore, in maxime architecto ab earum? Voluptatem odio illo consequatur
-                    distinctio consectetur minus eligendi provident unde totam ipsam maxime a reprehenderit exercitationem iste commodi atque dicta quaerat aliquam culpa labore ut, hic impedit assumenda.
-                    Ex iusto sequi quo pariatur enim unde deleniti minus exercitationem error reiciendis excepturi earum quae praesentium fuga modi impedit officia, sint, cumque aspernatur iste autem. Atque,
-                    repellendus praesentium ullam quasi expedita ab quibusdam? Blanditiis illum unde laborum, numquam eligendi aliquam laudantium est aspernatur, error optio, rem esse fugit. Nihil a maiores
-                    perspiciatis dolorem sapiente velit cumque totam dicta temporibus animi corporis delectus fugiat iure, modi molestias dolor enim magnam nemo necessitatibus repudiandae. Error voluptatem
-                    libero aliquid maiores quibusdam delectus culpa vel, asperiores nostrum, enim, esse placeat eveniet molestiae.
+                <p className=" text-defColors-dBG dark:text-defColors-lBG leading-loose">
+                    I'm Rubén Martínez, a full stack website developer with passion for cutting-edge technologies and a strong foundation in various programming languages, including JavaScipt, Typescript and Python.
+                    I consider myself a dynamic, responsible and decisive person, eager to learn and expand my knowledge.
+                    Throughout my journey, I have worked on diverse projects which allowed me to improve my skills in developing efficient and robust softwares.
+                    I'm exited to connect with like-minded people to team up and collaborate to create digital solutions and projects.
                 </p>
             </div>
         </div>
