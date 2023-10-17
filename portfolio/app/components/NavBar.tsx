f2ebae579e63ef118e22fa507e655ac28f6f8a5b
Rubén <98332317+marsi107@users.noreply.github.com>
2023-10-17 18:18:34 +0200

    Changed navBar sections for phone
    - remove resume and add home

--- portfolio/app/components/NavBar.tsx
+++ portfolio/app/components/NavBar.tsx
@@ -25,11 +25,11 @@ const NavBar = () => {
                 <Dropdown sections={menuSections} iconIndex={0} />
             </div>
             <div className="flex text-lg h-full items-center sm:text-2xl">
-                <a href="#Home" className="navBarItemsHover h-full pt-2 px-3 sm:px-5 lg:pt-3 lg:px-8 rounded-md max-sm:hidden" onClick={onSectionClick}>Home</a>
+                <a href="#Home" className="navBarItemsHover h-full pt-[10px] px-3 sm:px-5 lg:pt-3 lg:px-8 rounded-md" onClick={onSectionClick}>Home</a>
                 <a href="#AboutMe" className="navBarItemsHover h-full pt-2 px-3 sm:px-5 lg:pt-3 lg:px-8 rounded-md max-lg:hidden" onClick={onSectionClick}>About Me</a>
                 <a href="#TechStack" className="navBarItemsHover h-full pt-2 px-3 sm:px-5 lg:pt-3 lg:px-8 rounded-md max-lg:hidden" onClick={onSectionClick}>Tech Stack</a>
                 <a href="#Portfolio" className="navBarItemsHover h-full pt-[10px] px-3 sm:pt-2 sm:px-5 lg:pt-3 lg:px-8 rounded-md" onClick={onSectionClick}>Portfolio</a>
-                <a href="#Resume" className="navBarItemsHover h-full pt-[10px] px-3 sm:pt-2 sm:px-5 lg:pt-3 lg:px-8 rounded-md" onClick={onSectionClick}>Resume</a>
+                <a href="#Resume" className="navBarItemsHover h-full pt-[10px] px-3 sm:pt-2 max-sm:hidden sm:px-5 lg:pt-3 lg:px-8 rounded-md" onClick={onSectionClick}>Resume</a>
                 <a href="#Contact" className="navBarItemsHover h-full pt-[10px] px-3 sm:pt-2 sm:px-5 lg:pt-3 lg:px-8 rounded-md" onClick={onSectionClick}>Contact</a>
             </div>
             <div className='px-3 pt-1 sm:pl-5 lg:pt-[6px] lg:pl-8'>
