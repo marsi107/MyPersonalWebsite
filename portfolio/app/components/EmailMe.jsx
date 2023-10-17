327968c8dfe1e48bac80da5d426f9b5ff8b1a8ad
Rubén <98332317+marsi107@users.noreply.github.com>
2023-10-17 19:56:38 +0200

    Add some styling to Email form

--- portfolio/app/components/EmailMe.jsx
+++ portfolio/app/components/EmailMe.jsx
@@ -17,31 +17,39 @@ const EmailMe = () => {
     };
 
     return (
-        <form
-            className="block shadow-lg"
-            ref={form}
-            onSubmit={sendEmail}
-        >
-            <label>Name</label>
-            <input
-                className="text-defColors-dBG"
-                type="text"
-                name="user_name"
-            />
-            <label>Email</label>
-            <input
-                className="text-defColors-dBG"
-                type="email"
-                name="user_email"
-            />
-            <label>Message</label>
-            <textarea
-                className="text-defColors-dBG"
-                name="message"
-            />
-            <input type="submit" value="Send" />
-        </form>
-    );
-};
+        <div className="container max-w-lg shadow-lg font-rale bg-black bg-opacity-5 dark:bg-opacity-20 py-12 px-12 sm:px-24">
+            <h4 className="font-rale block text-center text-3xl font-extrabold mb-6">Email Me</h4>
+            <form
+                ref={form}
+                onSubmit={sendEmail}
+            >
+                <label className="block mt-4 mb-2">Name</label>
+                <input
+                    className="contactInputs"
+                    type="text"
+                    name="user_name"
+                    required
+                    placeholder="Your Name"
+                />
+                <label className="block mt-4 mb-2">Email</label>
+                <input
+                    className="contactInputs"
+                    type="email"
+                    name="user_email"
+                    required
+                    placeholder="Your Email"
+                />
+                <label className="block mt-4 mb-2">Message</label>
+                <textarea
+                    className="contactInputs sm:min-h-[100px]"
+                    name="message"
+                    required
+                    placeholder="Some Message..."
+                />
+                <input className="block cursor-pointer mt-6 bg-defColors-main rounded-lg py-1 w-full hover:text-defColors-lGray dark:hover:text-defColors-dGray" type="submit" value="Send" />
+            </form>
+        </div>
+    )
+}
 
 export default EmailMe
